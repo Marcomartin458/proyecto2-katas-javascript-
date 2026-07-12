fac7f19b6ac5be3cde88316ef66285fb85945384
Marco <marcomartinalvarez14@gmail.com>
2026-07-12 20:41:04 +0200

    ejercicios js terminados

---
+++ ejercicios_e6_asincronia/ejercicio-09-carpeta/ejercicio-09.js
@@ -0,0 +1,27 @@
+// Ahora realizaremos una petición a la PokeAPI, queremos mostrar al entrar en la página la imagen de un Pokemon, la magia estará en que cada vez que recargues la página, será un nuevo Pokemon dentro de la primera generación de Pokemon, es decir, del 1 al 151.
+
+// Los Pokemon no solo tienen una imagen, si no que tendrán muchas, hay que hallar la manera de encontrar la que más os guste.
+
+
+
+// Para ello el HTML será muy sencillo, y la URL esta vez os la aportaremos directamente, aunque os aconsejamos echarle un ojo a la documentación ya que es muy completa.
+
+// Documentación: https://pokeapi.co/
+
+// URL: https://pokeapi.co/api/v2/pokemon/1
+
+// Tened en cuenta que esta URL se refiere al pokemon número 1, que es bulbasaur, debemos hallar la manera de con una url similar ir consiguiendo pokemons aleatorios dentro de unos límites.
+
+const pokemonImage = document.getElementById("pokemon-image");
+
+// Generar un número aleatorio entre 1 y 151
+const randomPokemonId = Math.floor(Math.random() * 151) + 1;
+
+// Realizar la petición a la PokeAPI
+fetch(`https://pokeapi.co/api/v2/pokemon/${randomPokemonId}`)
+    .then(response => response.json())
+    .then(data => {
+        pokemonImage.src = data.sprites.front_default;
+    })
+    .catch(error => console.error('Error fetching data:', error));
+    
